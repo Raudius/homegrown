e447926769cb2c8029d28f0e1735a1b02736ecc3
raul <raul.ferreira.f@gmail.com>
2021-10-19 17:42:59 +0200

    Small update to interpreter to allow custom root environments

--- interpreter/interpreter.js
+++ interpreter/interpreter.js
@@ -3,12 +3,12 @@ const performActions = require('./actions').performActions;
 const Environment = require('./environment').Environment;
 
 class Interpreter {
-  executeProgram (program) {
+  executeProgram (program, environment = null) {
     console.time('Compilation');
     const actions = parser.parse(program);
     console.timeEnd('Compilation');
 
-    const rootEnvironment = this.createRootEnvironment();
+    const rootEnvironment = environment ?? this.createRootEnvironment();
 
     console.time('Runtime');
     performActions(rootEnvironment, actions);
